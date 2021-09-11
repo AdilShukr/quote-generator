d87c9e49aebf0bbdc16b6b4331d6b49b2082fdc6
Adil Shukr <adilrasheed7417@gmail.com>
2021-09-11 21:40:16 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { WhatsApp, Twitter } from '@material-ui/icons';
+import { useState , useEffect } from 'react';
+import axios from 'axios'; 
+import { BrowserRouter as Router ,Link } from 'react-router-dom';
+import './social-links'
+import links from './social-links';
 
-function App() {
+export default function App() {
+  const[quotes,setQuotes] = useState([]);
+
+  useEffect(() => {
+      async function fetchData(){
+          const url = await axios.get(links.api);
+          setQuotes(url.data[Math.floor(Math.random() * url.data.length)]);
+          return(url);
+
+      }
+      fetchData();
+  
+      
+  }, [])
+  const changequotes = () =>{
+      async function fetchData(){
+          const url = await axios.get(links.api);
+          setQuotes(url.data[Math.floor(Math.random() * url.data.length)]);
+          return(url);
+
+      }
+      fetchData();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <Router>
+            
+      <div className="quotes-box">
+          <div className="quotes-text">
+              <p>{quotes.text}</p>
 
-export default App;
+          </div>
+          <div className="quotes-author">
+              {quotes?.author }
+          </div>
+          <div className="buttons">
+              <div className="socialmedia">
+                 <Link to={{pathname:links.twitter}} target="_blank" className="social-icons"><Twitter/></Link>
+                  <Link to={{pathname:links.whatsapp}} target="_blank"className="social-icons"><WhatsApp/></Link>
+                </div>
+              
+              <button  onClick={changequotes} className="next-button">Next</button>
+           </div>
+      </div>
+      </Router>
+  )
+  
+  
+}
---
+++ src/social-links.js
@@ -0,0 +1,9 @@
+const links = 
+    {
+        linkedin: "https://platform.linkedin.com/in.js",
+        twitter: "https://twitter.com/intent/tweet",
+        whatsapp:"https://web.whatsapp.com",
+        api:"https://type.fit/api/quotes",
+    }
+
+export default links;
